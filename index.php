3bf4c5f750776f0d722da29d1d930fcb4c9691a3
kstyanoz <kstyanos@gmail.com>
2015-05-28 18:36:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -35,8 +35,8 @@ include 'configfb.php';
 
 		if(isset($_REQUEST["msj"]) AND isset($_REQUEST["tweet"]) AND $_REQUEST['tweet']==true){
 		
-			$consumerKey    = 'DWUXSI1l4posXOBkTJXTeC8Vt';
-			$consumerSecret = 'I5TLlFeKTOqBrEoy9xvNu60IoIq7OdwITowuUc5RD7gSYn7soM';
+			$consumerKey    = 'uzfFO6Rb3rEIsZqiPGddEQtGu';
+			$consumerSecret = 'liGm87lUgsKyK87TezyGPbUUXHbkCxok6DZPaFL12x4NrpSGg2';
 			$access_token     = '778752343-wP6EirICwRZiv755s2schp2b5WN4KEEmWTfja69w';
 			$access_token_secret    = 'gNeGXCIpSQMn8CjGi3TffSvDC3B4saarCbDxmkvEvE7rP';
 			# API OAuth
