c79f578a91b6c3542ac0ca4704f8cb873078cdb6
Michel <kstyanos@gmail.com>
2015-05-28 17:54:56 -0500

    Actualización de librerias twitter y upload media.

---
+++ configfb.php
@@ -0,0 +1,18 @@
+<?php
+	require_once( 'lib/Facebook/FacebookSession.php');
+	require_once( 'lib/Facebook/FacebookRequest.php' );
+	require_once( 'lib/Facebook/FacebookResponse.php' );
+	require_once( 'lib/Facebook/FacebookSDKException.php' );
+	require_once( 'lib/Facebook/FacebookRequestException.php' );
+	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
+	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
+	require_once( 'lib/Facebook/GraphObject.php' );
+	require_once( 'lib/Facebook/GraphUser.php' );
+	require_once( 'lib/Facebook/GraphSessionInfo.php' );
+	require_once( 'lib/Facebook/Entities/AccessToken.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
+	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
+
+	
+?>
